4f5b113434aaf4b058359088f0e0c24a3f30c1f3
Ben Vilakazi <xolanithengubane@gmail.com>
2024-01-08 23:07:24 +0200

    tragic

--- Doubl_Linked_Lists.c
+++ Doubl_Linked_Lists.c
@@ -154,3 +154,29 @@ void *List_remove(List * list, ListNode * node)
 error:
     return result:
 }
+
+
+#include "minunit.h"
+
+#include <assert.h>
+
+static List * list == NULL;
+char *test1 = "test1 data";
+char *test2 = "test2 data";
+char *test3 = "test3 data";
+
+char *test_create()
+{
+    list = List_create();
+    mu_assert(list != NULL, "Failed to create list");
+
+    return NULL;
+}
+
+char *test_destroy()
+{
+    List_clear_destroy(list);
+
+    return NULL;
+}
+
